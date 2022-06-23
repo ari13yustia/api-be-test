648997bf871e8b3771f8f4867f62c9ff2f263c9f
ari ys <ari13yustias@gmail.com>
2022-06-23 23:50:59 +0700

    final unit testing

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -13,10 +13,11 @@ class ApiTest extends TestCase
      *
      * @return void
      */
+    var $email = 'ariemail@gmail.com';
     public function test_register(){
         $data = [
             'name' => 'Ari',
-            'email' => 'ariemail@gmail.com',
+            'email' => $this->email,
             'password' => '12345',
             'password_confirmation' => '12345'
         ];
@@ -26,16 +27,37 @@ class ApiTest extends TestCase
 
     public function test_login(){
         $data = [
-            'email' => 'ariemail@gmail.com',
+            'email' => $this->email,
             'password' => '12345',
         ];
         $response = $this->post('api/login', $data);
         $response->assertStatus(201);
     }
 
-    // public function test_logout(){
-    //     $response = $this->post('api/logout');
-    //     $response->assertStatus(201);
-    // }
+    public function test_logout(){
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '.$this->createApiToken($this->email),
+            'Accept' => 'application/json',
+        ])->post('api/logout');
+        $response->assertStatus(200);
+    }
+
+    public function test_search_province(){
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '.$this->createApiToken($this->email),
+            'Accept' => 'application/json',
+        ])->get('api/search_swap/province', ['id' => '3']);
+        $response = $this->withoutExceptionHandling()->get('api/search_swap/province', ['id' => '3']);
+        $response->assertStatus(200);
+    }
+
+    public function test_search_city(){
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '.$this->createApiToken($this->email),
+            'Accept' => 'application/json',
+        ])->get('api/search_swap/city', ['id' => '3']);
+        $response = $this->withoutExceptionHandling()->get('api/search_swap/city', ['id' => '3']);
+        $response->assertStatus(200);
+    }
 
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Psy\Util\Str;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $user;
+
+    public function createApiToken($email){
+        $user = User::where('email', $email)->first();
+        return $user->createToken('myapptoken')->plainTextToken;
+    }
 }
