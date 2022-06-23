ade1207c0b1dc5203bbfae1b731a22edb9549007
ari ys <ari13yustias@gmail.com>
2022-06-23 14:08:10 +0700

    add route seach

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/search/province', [App\Http\Controllers\CariDataController::class, 'searchDataProvince']);
+Route::get('/search/city', [App\Http\Controllers\CariDataController::class, 'searchDataCity']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
